09ea58ddf487eb30a791082e02c8a2ec19460820
Philipp Schumacher <p.schumacher@auckland.ac.nz>
2016-09-18 13:42:23 +1200

    logging

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,11 +17,13 @@ import java.util.Map;
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     private Map<Long, User> userMap = new HashMap<>();
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public ResponseEntity<User> get(@PathVariable long id) {
+        logger.info("GET was called on user " + id);
         User user = userMap.get(id);
         if(user != null) {
             return new ResponseEntity<>(user, HttpStatus.OK);
