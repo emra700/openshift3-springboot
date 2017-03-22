38319628d701ef59f54031b4827c4df0be55e623
Emil Ramstedt <emira@nets.eu>
2017-03-22 16:17:51 +0100

    OK

--- src/main/java/com/example/UserController.java
+++ src/main/java/com/example/UserController.java
@@ -2,29 +2,20 @@ package com.example;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-
 @RestController
-@RequestMapping("/user")
 public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 
-
-    @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String get(@PathVariable long id) {
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String get() {
         return "hello";
     }
-
 
 
 }
--- src/main/resources/db/migration/V1_0__user.sql
+++
@@ -1,5 +0,0 @@
-create table atp.user (
-  id bigint NOT NULL AUTO_INCREMENT,
-  name varchar(100) NOT NULL,
-  PRIMARY KEY (id)
-);
